0cd9ca7729e94b8efdb97de6bac2faaa05de10ac
dddd-onigiri <dddd.sailing470@gmail.com>
2023-01-24 23:15:16 +0900

    tuiks

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import streamlit as st
 
-st.title("BMI Calculator")
+st.title("BMI 計算機（Streamlitデモ）")
 
-height_cm = st.number_input("Height in cm", min_value=1, max_value=250)
+height_cm = st.number_input("身長（cm）", min_value=1, max_value=250)
 
-weight_kg = st.number_input("Weight in kg", min_value=1, max_value=250)
+weight_kg = st.number_input("体重（kg）", min_value=1, max_value=250)
 
-bmi = weight_kg/(height_cm/100)**2
+bmi = weight_kg / (height_cm / 100) ** 2
 
-st.write("Your BMI is", bmi)
+st.write("あなたのBMIは", bmi, "です。")
 
-if bmi<18.5:
+if bmi < 18.5:
     st.write("やせ型です")
-elif bmi<24.9:
+elif bmi < 24.9:
     st.write("標準です")
-elif bmi<29.9:
+elif bmi < 29.9:
     st.write("肥満気味です")
 else:
     st.write("肥満です")
