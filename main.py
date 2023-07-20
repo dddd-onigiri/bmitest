cab1135148287b23273000aed9f5a2cd5debbbd3
Daiki Ito <39673489+dddd-onigiri@users.noreply.github.com>
2023-07-20 15:56:26 +0900

    aaa

--- main.py
+++ main.py
@@ -26,3 +26,27 @@ n = st.number_input("n")
 
 st.write("pのn乗は")
 st.write(str(p**n))
+
+from sympy import Symbol, Eq, solve
+
+def solve_linear_equation(equation_str):
+    x = Symbol('x')  # 変数 x を定義
+
+    # 方程式を作成
+    equation = Eq(eval(equation_str), 0)
+
+    # 方程式を解く
+    solutions = solve(equation, x)
+
+    return solutions
+
+# 一次不定方程式を解く
+equation_str = input("一次不定方程式を入力してください（例: 2*x + 3 = 0）: ")
+solutions = solve_linear_equation(equation_str)
+
+if solutions:
+    print("解:")
+    for sol in solutions:
+        st.write(f"x = {sol}")
+else:
+    st.write("解が見つかりませんでした。")
